194d6d9f953e09feffff45110efe8b9bde628850
Mason Chinkin <mason.chinkin@gmail.com>
2019-01-22 17:13:41 -0800

    Solved in sudocode using too much hard-coding, needs to be made more dynamic

--- solution.rb
+++ solution.rb
@@ -8,7 +8,21 @@ def valid_board?
 end
 
 def squares
-  squares = []
-  @grid.each do |row|
+  squares = Array.new(9) { [] }
+
+  @grid.each_with_index do |row, row_i|
+    row.each_with_index do |col, col_i|
+      if row_i < 3 && col_i < 3
+        squares[0] << col
+      end
+
+      if row_i < 3 && col_i < 6 && col_i > 2
+        squares[1] << col
+      end
+
+      #...
+    end
   end
+
+  squares
 end
